c59b585aa8a14d631af4aa76734c29abde20cedc
Christiaan Dirkx <christiaan@dirkx.email>
2020-11-30 23:11:55 +0100

    Simplify .NET impl
    Remove `Record` and use an anonymous record instead.

--- impls/dotnet/IPCheck.cs
+++ impls/dotnet/IPCheck.cs
@@ -2,36 +2,20 @@
 using System.Net;
 using System.Text.Json;
 
-class Program
+class IPCheck
 {
     static void Main(string[] args)
     {
-        var addr = args[0];
+        var input = args[0];
+        var addr = IPAddress.Parse(input);
 
-        Console.WriteLine("{0}", JsonSerializer.Serialize<Result>(Check(addr)));
-    }
-
-    static Result Check(string addr)
-    {
-        var ipAddr = IPAddress.Parse(addr);
-
-        var asV4 = ipAddr.MapToIPv4();
-        var asV6 = ipAddr.MapToIPv6();
-
-        return new Result
-        {
-            input = addr,
-            rendered = ipAddr.ToString(),
-            asV4 = asV4.ToString(),
-            asV6 = asV6.ToString(),
+        var data = new {
+            input = input,
+            rendered = addr.ToString(),
+            asV4 = addr.MapToIPv4().ToString(),
+            asV6 = addr.MapToIPv6().ToString(),
         };
-    }
-}
 
-record Result
-{
-    public string input { get; init; }
-    public string rendered { get; init; }
-    public string asV4 { get; init; }
-    public string asV6 { get; init; }
+        Console.WriteLine("{0}", JsonSerializer.Serialize(data));
+    }
 }
