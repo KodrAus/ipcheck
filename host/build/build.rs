488989879fe20ea3c3a6773d4caadc800bf2ccee
Christiaan Dirkx <christiaan@dirkx.email>
2020-11-30 23:22:25 +0100

    Emit `rerun-if-changed` during build

--- host/build/build.rs
+++ host/build/build.rs
@@ -2,16 +2,27 @@ mod rust;
 mod dotnet;
 
 fn main() {
-    if !std::path::Path::new("../../artifacts").exists() {
-        std::fs::create_dir("../../artifacts").expect("failed to create artifacts dir");
+    if !std::path::Path::new("../artifacts").exists() {
+        std::fs::create_dir("../artifacts").expect("failed to create artifacts dir");
     }
 
     handle_err("Rust",rust::build());
     handle_err(".NET", dotnet::build());
+
+    rerun_if_changed("build");
+    rerun_if_changed("../impls");
 }
 
 fn handle_err(lang: &str, result: std::io::Result<()>) {
     if let Err(err) = result {
         panic!("Failed to build {} impl: {}", lang, err)
+    }
+}
+
+fn rerun_if_changed(dir: &str) {
+    for entry in walkdir::WalkDir::new(dir) {
+        if let Ok(entry) = entry {
+            println!("cargo:rerun-if-changed={}", entry.path().display());
+        }
     }
 }
